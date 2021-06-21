bfe00c9003f4ef7020e6599cb7b8547a32a6d969
issacMontes28 <meio139602@upemor.edu.mx>
2021-06-21 18:54:20 -0500

    model for images table

--- app/Image.php
+++ app/Image.php
@@ -4,7 +4,27 @@
 namespace App;
 
 
-class Image
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'url'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'updated_at', 'deleted_at'
+    ];
+
 
 }
