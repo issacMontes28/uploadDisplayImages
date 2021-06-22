b3486b273de0b5b389f137667e64d962db8cf1cb
issacMontes28 <meio139602@upemor.edu.mx>
2021-06-21 19:07:20 -0500

    vue for add and delete images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+Vue.component(
+    'image-loader',
+    require('./components/ImageLoader.vue').default
+);
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+});
